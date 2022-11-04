e41b471c3b75d430e3446d3acac9e28427fbc749
arb-tahiri <arb.tahiri@dataprognet-ks.com>
2022-11-04 16:35:16 +0100

    Improvements

--- AMS/Models/Shared/ErrorVM.cs
+++ AMS/Models/Shared/ErrorVM.cs
@@ -5,16 +5,10 @@ namespace AMS.Models.Shared;
 public class ErrorVM
 {
     public string Title { get; set; }
-
     public ErrorStatus Status { get; set; }
-
     public string Description { get; set; }
-
     public bool RawContent { get; set; }
-
     public string Icon { get; set; }
-
     public bool Dismissible { get; set; }
-
     public bool Light { get; set; }
 }
